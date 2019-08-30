8922a5555c79e497216509aa136220d9b3aa3cf3
bookmanindia <bookmanonmobile@gmail.com>
2019-08-30 11:26:49 +0530

    Add new book

--- README.md
+++
@@ -1,1 +0,0 @@
-# Learning-Science-class-01
---
+++ files/pageConfig/app.js
@@ -0,0 +1,71 @@
+function populate() {
+    if(quiz.isEnded()) {
+        showScores();
+    }
+    else {
+        // show question
+        var element = document.getElementById("question");
+        element.innerHTML = quiz.getQuestionIndex().text;
+
+        // show options
+        var choices = quiz.getQuestionIndex().choices;
+        for(var i = 0; i < choices.length; i++) {
+            var element = document.getElementById("choice" + i);
+            element.innerHTML = choices[i];
+            guess("bt" + i, choices[i]);
+        }
+
+        showProgress();
+    }
+};
+
+function guess(id, guess) {
+    var button = document.getElementById(id);
+    button.onclick = function() {
+        quiz.guess(guess);
+        populate();
+    }
+};
+
+
+function showProgress() {
+    var currentQuestionNumber = quiz.questionIndex + 1;
+    var element = document.getElementById("progress");
+    element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
+};
+
+function showScores() {
+    var gameOverHTML = "<h1>Result</h1>";
+    gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
+    var element = document.getElementById("quiz");
+    element.innerHTML = gameOverHTML;
+};
+
+// create questions
+var questions = [
+    new Question("Our skin helps us to __________.", ["hear", "see", "feel"], "feel"),
+
+    new Question("Our eyes help us to __________.", ["see", "hear","write"], "see"),
+    
+
+    new Question("Our ears help us to __________.", ["write", "taste","hear"], "hear"),
+    
+
+    new Question("Our tongue helps us to __________. ", ["eat", "smell","taste"], "taste"),
+
+
+  new Question("Our nose helps us to __________. ", ["eat", "smell","run"], "smell"),
+        
+
+];
+
+
+var quiz = new Quiz(questions);
+
+
+populate();
+
+
+
+
+
